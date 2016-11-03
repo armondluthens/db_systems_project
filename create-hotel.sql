d75ef3c9abf74d4c59b5f9749d665b53005160b4
Bryan Prather-Huff <pratherhuff@gmail.com>
2016-11-03 11:19:42 -0500

    starting sql schema

---
+++ create-hotel.sql
@@ -0,0 +1,55 @@
+create table Customer (
+  cid int PRIMARY KEY,
+  first_name varchar(20),
+  last_name varchar(20),
+  phone varchar(10)
+);
+
+create table Room_Type (
+  room_type int PRIMARY KEY,
+  no_people int,
+  cost int
+);
+
+create table Room (
+  room_id int PRIMARY KEY,
+  occupied_status boolean,
+  room_type int REFERENCES Room_Type
+);
+
+create table Transaction (
+  transaction_id int PRIMARY KEY,
+  amount int,
+  payment_method varchar(20),
+  payment_date timestamp,
+  refund_amount int
+);
+
+create table Employee (
+  employee_id int PRIMARY KEY,
+  first_name varchar(20),
+  last_name varchar(20),
+  position varchar(20)
+);
+
+create table Reservation (
+    reservation_id int PRIMARY KEY,
+   cid int REFERENCES Customer,
+   room_id int REFERENCES Room,
+   reservation_date timestamp,
+   check_in_date timestamp,
+   check_out_date timestamp,
+   checked_in_status boolean,
+   checked_out_status boolean,
+   transaction_id int REFERENCES Transaction
+);
+
+create table House_Keeping (
+  room_id int REFERENCES Room,
+  date_of_service timestamp,
+  assigned_by_id int REFERENCES Employee,
+  assigned_to_id int REFERENCES Employee,
+  description varchar(500),
+  completion_status boolean,
+  PRIMARY KEY(room_id, date_of_service)
+);
